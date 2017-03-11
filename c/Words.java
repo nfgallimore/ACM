0a2f78ffe943c9402eafc427c172fa7f8cc3e816
nfg3@zips.uakron.edu <nfg3@zips.uakron.edu>
2017-03-11 18:07:46 -0500

    C works boy

--- c/Words.java
+++ c/Words.java
@@ -1,13 +1,18 @@
 import java.io.*;
-
+import java.util.*;
 public class Words {
-    public static void main(){
+    public static void main(String[] args){
 	Scanner scanner = new Scanner(System.in);
 	int numBadChar = scanner.nextInt();
 	int startPoint = scanner.nextInt();
 	int numWords = scanner.nextInt();
-	String s = scanner.nextLine();
-	char[] badLetters = char[numBadChar];
+	String s = "";
+	int tmp = numBadChar;
+	while (tmp > 0){
+	    s = scanner.next();
+	    tmp--;
+	}
+	char[] badLetters = new char[numBadChar];
 	for (int i = 0; i < s.length(); i++) {
 	    badLetters[i] = s.charAt(i);
 	}
@@ -16,23 +21,24 @@ public class Words {
 
 	char[] alph = new char[numUsableChar];
 
-	char[] standardAlph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-	int i=0;
-	int j=0;
-	bool add = true;
+	char[] standardAlph = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
+			       'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	int i1=0;
+	int j1=0;
+	boolean boolAdd = true;
 
-	while(i<numUsableChar){
-	    for int(k=j; k<numBadChar; k++){
-		    if(standardAlph[i+j]==badLetters[k]){
+	while(i1<numUsableChar){
+	    for (int k=j1; k<numBadChar; k++){
+		    if(standardAlph[i1+j1]==badLetters[k]){
 			boolAdd=false;
-			j++;
+			j1++;
 			k=numBadChar;
 		    }
 		}
 	    if(boolAdd==true){
-		alph[i] = standardAlphabet[i+j];
+		alph[i1] = standardAlph[i1+j1];
 	    }
-	    i++;
+	    i1++;
 	    boolAdd=true;
 	}
 	
@@ -41,25 +47,31 @@ public class Words {
 	for(int i=0; i<numUsableChar; i++){
 	    for(int j=0; j<numUsableChar; j++){
 		for(int k=0; k<numUsableChar; k++){
-		    for(int k=0; k<numUsableChar; k++){
-			if(start>=numStart && maxCatch>0){
-			    System.out.println( Alph[i] + Alph[j] + Alph[k] + Alph[l]);
+		    for(int l=0; l<numUsableChar; l++){
+			if(start>=startPoint && maxCatch>0){
+			    
+			     System.out.println(""
+					       + standardAlph[i] 
+					       + standardAlph[j] 
+					       + standardAlph[k] 
+					       + standardAlph[l]
+					       );
 			    maxCatch--;
 			}
 			start++;
 
 			//if number of output reached break from all for loops
 			if(maxCatch<=0){
-			    i=numUsable;
-			    j=numUsable;
-			    k=numUsable;
-			    l=numUsable;
+			    i=numUsableChar;
+			    j=numUsableChar;
+			    k=numUsableChar;
+			    l=numUsableChar;
 			}
 		    }
 		}
 	    }
 	}
 	    
-	
+    }
     
 }
