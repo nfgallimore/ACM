64d21f186379edc779ae9af2d152007ea467cdbd
Nicholas Gallimore <nfgallimore@gmail.com>
2017-03-11 20:16:54 -0500

    Update Polish.java

--- b/Polish.java
+++ b/Polish.java
@@ -2,27 +2,72 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-public class Polish {
-    public static void main(String[] args) {
+public class Polish 
+{
+    public static void main(String[] args)
+    {
 	Scanner scan = new Scanner(System.in);
-	int n = 0;
-	if (scan.hasNextInt()) {
-	    int tmpn = scan.nextInt();
-	    if (1 <= tmpn && tmpn <= 25) n = tmpn;
+
+	int n = null;
+	
+	if (scan.hasNextInt())
+	{
+		int tmpn = scan.nextInt();
+		if (1 <= tmpn <= 25) 
+	    n = scan.nextInt();
 	}
-	String s = "";
+	
+	String str = scan.nextLine();
+
+	String operStack;
+	String digitStack;
+	for (int i = n-1; i > 0; i--)
+	{
+		if (str.charAt(i) == '+') {
+			operStack += '+';
+		}
+		else if (str.charAt(i) == '-') {
+			operStack += '-';
+		}
+		else if (str.charAt(i) == '*') {
+			operStack += '*';
+		}
+		else if (str.charAt(i) == '/') {
+			operStack += '/';
+		}
+		else if (strArr.charAt(i).isDigit()) {
+			digitStack += strArr.charAt(i);
+		}
+	}
+	int operIteratorCount = 0;
+	String out = "";
+	while (operIteratorCount < operStack.length()) {
+		if (operStack.charAt(digitStack.charAt(operIteratorCount)) == '+') {
+			operIteratorCount++;
+		}
+		if (operStack.charAt(digitStack.charAt(operIteratorCount)) == '-') {
+			operIteratorCount++;
+		}
+		if (operStack.charAt(digitStack.charAt(operIteratorCount)) == 'x') {
+			operIteratorCount++;
+		}
+		if (operStack.charAt(digitStack.charAt(operIteratorCount)) == '/') {
+			operIteratorCount++;
+		}
+	while (digitIteratorCount < operStack.length()) {
+			out += digitStack.charAt(operDigitCount) / digitStack.charAt(operDigitCount+1);
+			operDigitCount++;
+		}
+	}
 	int numVals = n;
 	while (numVals > 0) {
 	    String tmp = scan.next();
-	    if ((numVals - n) % 2 == 1) {
-		if (1 <= tmp.charAt(numVals) && tmp.charAt(numVals) <= 9) {
-		    s += tmp;
-		    numVals--;
-		}
-	    }
-	    else {
+	    if (1 <= tmp.charAt(numVals) && tmp.charAt(numVals) <= 9) {
 		s += tmp;
 		numVals--;
+	    }
+	    else {
+		r
 	    }
 	    
 	}
